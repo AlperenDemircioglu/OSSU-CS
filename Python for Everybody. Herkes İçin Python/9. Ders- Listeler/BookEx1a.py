1755eec7040dd1d6f87744823cb38307ad28ed17
AD-Neq <61984574+AlperenDemircioglu@users.noreply.github.com>
2022-06-12 19:38:34 +0300

    Lesson 9-List exercise 1. I could not find a way to use input statement to get lists like this [1,2,3,4] and use them in the arg of a function.
    Instead i created the list in the program itself.

---
+++ Python for Everybody. Herkes İçin Python/9. Ders- Listeler/BookEx1a.py
@@ -0,0 +1,8 @@
+def chop(arg):
+    result = arg[1:-1]
+    return None
+
+items = ["book", "notebook", "pencil", "watch", "glass", "table"]
+
+
+print(chop(items))
---
+++ Python for Everybody. Herkes İçin Python/9. Ders- Listeler/BookEx1b.py
@@ -0,0 +1,7 @@
+def middle(arg):
+    result = arg[1:-1]
+    return result
+
+items = ["book", "notebook", "pencil", "watch", "glass", "table"]
+
+print(middle(items))
