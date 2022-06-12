72633d26fdcb2d6bbaa1928912ffd287a4e971cc
AD-Neq <61984574+AlperenDemircioglu@users.noreply.github.com>
2022-06-12 21:38:41 +0300

    work

--- Python for Everybody. Herkes İçin Python/9. Ders- Listeler/BookEx1a.py
+++ Python for Everybody. Herkes İçin Python/9. Ders- Listeler/BookEx1a.py
@@ -1,3 +1,9 @@
+'''
+Exercise 1: Write a function called chop that takes a list and modifies it, removing the first and last elements, and returns None.
+Then write a function called middle that takes a list and returns a new list that contains all but the first and last elements.
+'''
+
+
 def chop(arg):
     result = arg[1:-1]
     return None
